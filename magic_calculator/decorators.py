1cadfeb39224adbe1a20b6ec0bfb814c63f0af6c
soumen448 <100519288+soumen448@users.noreply.github.com>
2022-02-27 19:27:14 +0530

    Add files via upload

--- magic_calculator/decorators.py
+++ magic_calculator/decorators.py
@@ -1,12 +1,16 @@
 import random
 import magic_calculator.functions as f
 def magic(a,b):
-    num = random.rarandint(1,4)
+    num = random.randint(1,4)
     if num == 1:
+        print("Magic Operation: Addition")
         return f.add(a,b)
     elif num == 2:
+        print("Magic Operation: Subtraction")
         return f.sub(a,b)
     elif num == 3:
+        print("Magic Operation: Multiplication")
         return f.mult(a,b)
     elif num == 4:
+        print("Magic Operation: Division")
         return f.div(a, b)
